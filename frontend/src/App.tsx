f6dfce54d1941f86fb4d7d0f1e8cf9b700c82a1a
John-Anthony <johnanthonyelenis@gmail.com>
2025-12-21 11:08:53 -0500

    added github link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import HomePage from './components/home/HomePage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GiWhiteBook } from 'react-icons/gi';
+import { FaGithub } from "react-icons/fa";
+
 
 const queryClient = new QueryClient();
+
 
 /**
  * Root component wrapping the app with React Query provider.
@@ -10,16 +13,21 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+
+      {/* Container will center the content and provide maximum width */}
       <div className='container'>
         <h1 className="page-title">
             Decode Book <GiWhiteBook />
         </h1>
         <h2 className="page-subtitle">AI Powered Electrical Code</h2>
+
+        {/* HomePage contains the main application */}
         <HomePage />
       </div>
 
+      {/* Webpage footer */}
       <div className='page-footer'>
-        Created by John Elenis
+        Created by John Elenis <a href='https://github.com/jelenis'><FaGithub/></a>
       </div>
     </QueryClientProvider>
   );
